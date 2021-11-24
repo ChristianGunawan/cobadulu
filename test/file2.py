ff605a5e85109ea0890e1df6fd5affda984d6ee0
mizuday <mizuday5.7@gmail.com>
2021-11-24 18:14:22 +0700

    add test cases

---
+++ test/file2.py
@@ -0,0 +1,25 @@
+class method(ibasdasd):
+    def funcname(self, parameter_list):
+        pass
+variavleBoolean = True
+if (not(variavleBoolean)):
+    while(1):
+        if True:
+            break
+        elif False:
+            pass
+        else:
+            fr = "ddwdwdw"
+            var = 100
+            print(fr+'frfe')
+            print(fr, sd, sdasd, var)
+def function(sentences, b):
+    c = False
+    if (len(sentences) > 5 and b) or c:
+        pass
+    else :
+        i = len(sentences)
+        for i in range(len(a) + 100):
+            print(i)
+            if (i > 10 and (i % 10 == 1)):
+                continue
